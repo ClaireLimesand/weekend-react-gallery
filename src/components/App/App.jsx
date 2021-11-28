2e50fa810e17c42edc7e02c4c6545aeca1a09cd2
Claire Limesand <clairelimesand@gmial.com>
2021-11-28 10:00:43 -0600

    worked through app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,40 @@
 import React from 'react';   
 import {useState, useEffect} from 'react';
 import axios from 'axios';
-
 import './App.css';
 
+function App() {
 
+  const [galleryList, setGalleryList] = useState([]);
+  
+  useEffect(() => {
+    fetchGallery();
+  }, []);
+
+  const fetchGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      console.log('got a response. Here\'s the data', response.data);
+
+      setPicsList(response.data);
+    }).catch((error) => {
+      console.log('GET /gallery failed', error);
+    });
+  };
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+        <p>Gallery goes here</p>
+        <img src="images/goat_small.jpg"/>
+    </div>
+  );
+
+}
 
 export default App;
 
