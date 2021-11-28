c221c82c07d1a7fecab16f22a9db242baefa1283
Claire Limesand <clairelimesand@gmial.com>
2021-11-28 09:44:50 -0600

    added files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,10 @@
-import React from 'react';
+import React from 'react';   
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
-}
+
 
 export default App;
+
